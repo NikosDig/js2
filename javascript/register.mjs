531bcb7086976bfc3a11883e80c978666662e93a
NikosDigalakis <nikos_23_chania@hotmail.com>
2023-07-11 19:15:52 +0200

    added documentation
    added doc for the registerUser function and cleared the console logs

--- javascript/register.mjs
+++ javascript/register.mjs
@@ -2,15 +2,25 @@ import { API_URL } from "./functions.mjs";
 
 const registerForm = document.querySelector(".registerForm");
 
+/**
+ * main function that creates the profiile object which is added to the api call
+ * to create a new user
+ */
 registerForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const form = e.target;
   const formData = new FormData(form);
-  const profile = Object.fromEntries(formData.entries());
+  const profile = Object.fromEntries(formData.entries()); //stolen line from Oliver
 
   registerUser(profile);
 });
 
+/**
+ *
+ * @param {object} profile
+ * takes the data from the form entries and makes an asynchronous
+ * call to the API to register a new user
+ */
 async function registerUser(profile) {
   const url = API_URL + "/auth/register";
 
@@ -23,5 +33,4 @@ async function registerUser(profile) {
   });
 
   const data = await response.json();
-  console.log(data);
 }
