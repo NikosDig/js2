ea6ecaed827150909a21ae225736ca620fd61207
NikosDigalakis <nikos_23_chania@hotmail.com>
2023-09-10 13:08:27 +0200

    fixed external dependecies in a single file

---
+++ javascript/externalDep.mjs
@@ -0,0 +1,24 @@
+//code imported from bootstrap for adding custom validation to the forms
+// bootstrap form validation
+export const externalDependecy = (async () => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
